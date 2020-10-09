950abfbff0d3ecd67435c066c291b0fb0f356993
Chandima Ranaweera <chan4lk@gmail.com>
2020-10-09 20:21:45 +0530

    push to azure storage

--- apps/admin/src/environments/environment.prod.ts
+++ apps/admin/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: true,
+  baseUrl: 'https://dieticianweb.azurewebsites.net',
+  auth: 'Auths',
+  users: 'Users',
+  diet: 'Diet',
+  foods: 'Foods',
+  progress: 'api/Progress',
+  profile: 'Profiles',
 };
