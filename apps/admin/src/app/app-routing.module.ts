925a082fab094dc7d74e7ef725f59b2d1c74f423
Chandima Ranaweera <chan4lk@gmail.com>
2020-10-09 15:42:05 +0530

    add routing

---
+++ apps/admin/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('@my-diet-admin/dashboard').then((m) => m.DashboardModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
